17711f3cf1f283a9ab4c9b2aff9409e54fc9c175
Timur Safin <timur.safin@gmail.com>
2016-02-29 02:14:21 +0300

    found a way to calculate local commits which are not in upstream

--- dirty-auto-merge.bat
+++ dirty-auto-merge.bat
@@ -1,14 +1,13 @@
 @echo off
+setLocal EnableDelayedExpansion
 pushd .git-staging
 for %%i in (intersystems,intersystems-ru) do (
   pushd %%i
   for /D %%j in (*) do (
     pushd %%j
-    git log --oneline -n1 | findstr "merge remote-tracking" > nul
-    if not errorlevel 1 (
-      echo %%i\%%j
-      rem rmdir /s/q .
-    )
+    rem git log --oneline -n1 | findstr "merge remote-tracking" > nul
+    echo %%i\%%j:
+    git log --branches --not --remotes=upstream --oneline
     popd
   )
   popd
